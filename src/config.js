3a441f278d4566da37841a115c5e4edf702722f5
Chaim Krause <chaim@chaim.com>
2021-07-19 06:51:35 -0500

    Update config.js
    Points at my project

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ const getQuery = (query) => {
 }
 
 const query = getQuery({});
-const PROJECT_URL = query.projectUrl || 'https://reactstarter.form.io';
+var PROJECT_URL = 'https://qnefxammpwmygsq.form.io';
 const API_URL = query.apiUrl || 'https://api.form.io';
 
 export const AppConfig = {
